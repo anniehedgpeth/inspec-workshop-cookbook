9e7584888b959494c8a70fccfca0eb989b9f282f
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-05-25 20:15:50 -0500

    metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'inspec-workshop-cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Annie Hedgpeth'
+maintainer_email 'annie.hedgpeth@gmail.com'
 license 'all_rights'
 description 'Installs/Configures inspec-workshop-cookbook'
 long_description 'Installs/Configures inspec-workshop-cookbook'
