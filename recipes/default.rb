f378189246a98d36d796db6fdb0f92066a9c369d
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-06-04 16:33:27 -0500

    added files

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,28 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-package "git"
+cookbook_file '/etc/grub.conf' do
+  source 'grub.conf'
+  action :create
+end
 
-log "Well, that was too easy"
+cookbook_file '/etc/security/limits.conf' do
+  source 'limits.conf'
+  action :create
+end
+
+cookbook_file '/etc/sysctl.conf' do
+  source 'sysctl.conf'
+  action :create
+end
+
+directory '/etc/sysconfig'
+
+cookbook_file '/etc/sysconfig/init' do
+  source 'init'
+#  action :create <----don't really need it because create is the default setting for actions
+end
+
+package 'ntp'
+package 'ntpdate'
+package 'ntp'
